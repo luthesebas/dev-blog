b25270116c799a91516ee055089bfd754ebf0539
luthesebas <luthesebas@gmail.com>
2021-10-22 15:10:05 +0200

    inline html and scss

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,8 +2,11 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  template: `
+    <h1>Welcome to {{title}}!</h1>
+    <router-outlet></router-outlet>
+  `,
+  styles: []
 })
 export class AppComponent {
   title = 'dev-blog';
