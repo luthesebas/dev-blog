26d9439a1a57214b03d434ba30ec85004a154e2a
luthesebas <luthesebas@gmail.com>
2021-10-22 16:47:19 +0200

    add scully blog support

--- scully.dev-blog.config.ts
+++ scully.dev-blog.config.ts
@@ -4,5 +4,11 @@ export const config: ScullyConfig = {
   projectName: "dev-blog",
   outDir: './dist/static',
   routes: {
+    '/blog/:slug': {
+      type: 'contentFolder',
+      slug: {
+        folder: "./blog"
+      }
+    },
   }
 };
