225a48fc49123b277c6f0284da68ab46b810a259
luthesebas <luthesebas@gmail.com>
2021-10-30 01:39:32 +0200

    Move HomeComponent in its own folder

--- src/app/pages/home/home.module.ts
+++ src/app/pages/home/home.module.ts
@@ -1,9 +1,8 @@
-import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
 
 import { HomeRoutingModule } from './home-routing.module';
-import { HomeComponent } from './home.component';
-
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
