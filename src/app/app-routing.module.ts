e4d1e399d4eb8fad2fe966e1a99741df52464724
luthesebas <luthesebas@gmail.com>
2021-11-01 16:04:28 +0100

    Restructure blog module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,7 +4,7 @@ import { RouterModule, Routes } from '@angular/router';
 const routes: Routes = [
   {
     path: 'blog',
-    loadChildren: () => import('./blog/blog.module').then(m => m.BlogModule)
+    loadChildren: () => import('./shared/blog/blog.module').then(m => m.BlogModule)
   },
   {
      path: '',
