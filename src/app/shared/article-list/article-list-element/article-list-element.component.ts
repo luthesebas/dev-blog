a5cc29abe1c52b51b17d0551e97748fa6835d53b
luthesebas <luthesebas@gmail.com>
2021-11-01 17:48:06 +0100

    Show article basic

--- src/app/shared/article-list/article-list-element/article-list-element.component.ts
+++ src/app/shared/article-list/article-list-element/article-list-element.component.ts
@@ -1,15 +1,16 @@
-import { Component, OnInit } from '@angular/core';
+import { ChangeDetectionStrategy, Component, Input, OnInit } from '@angular/core';
+
+import { Article } from 'src/app/core/article.interface';
 
 @Component({
   selector: 'app-article-list-element',
   templateUrl: './article-list-element.component.html',
-  styleUrls: ['./article-list-element.component.scss']
+  styleUrls: ['./article-list-element.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class ArticleListElementComponent implements OnInit {
+export class ArticleListElementComponent {
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
+  @Input()
+  article!: Article;
 
 }
