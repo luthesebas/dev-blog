a75a793633101873a07020c43a57420833c078cc
luthesebas <luthesebas@gmail.com>
2021-11-01 16:28:40 +0100

    Create ArticleListComponent

--- src/app/shared/article-list/article-list.module.ts
+++ src/app/shared/article-list/article-list.module.ts
@@ -1,10 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ArticleListComponent } from './article-list/article-list.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    ArticleListComponent
+  ],
   imports: [
     CommonModule
   ]
