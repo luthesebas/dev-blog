a78ffbac6eac282dfb5b56564e6c02e34b799b6d
luthesebas <luthesebas@gmail.com>
2021-11-03 15:43:00 +0100

    Show article tags via chips

--- src/app/shared/article-list/article-list.module.ts
+++ src/app/shared/article-list/article-list.module.ts
@@ -2,6 +2,7 @@ import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
+import { MatChipsModule } from '@angular/material/chips';
 import { RouterModule } from '@angular/router';
 
 import { ArticleListElementComponent } from './article-list-element/article-list-element.component';
@@ -17,6 +18,7 @@ import { ArticleListComponent } from './article-list/article-list.component';
     RouterModule,
     MatButtonModule,
     MatCardModule,
+    MatChipsModule
   ],
   exports: [
     ArticleListComponent
